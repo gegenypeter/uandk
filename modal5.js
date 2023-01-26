4a78de98c5840b07960dbb9995035ee885aabbbb
GegenyPeter <gegeny.p@gmail.com>
2023-01-26 13:01:42 +0100

    .

---
+++ modal5.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  var modal = document.getElementById("myModal");
+
+  var img1 = document.getElementById("img1");
+  var img2 = document.getElementById("img2");
+  var img3 = document.getElementById("img3");
+  var img4 = document.getElementById("img4");
+  var img5 = document.getElementById("img5");
+
+  var modalImg = document.getElementById("img01");
+
+  var span = document.getElementsByClassName("close")[0];
+  
+  img1.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img2.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img3.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img4.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img5.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+});
