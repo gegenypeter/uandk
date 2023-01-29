5a13ea7cff6b008fce8e20c7727ed6700e4f3bec
GegenyPeter <gegeny.p@gmail.com>
2023-01-29 11:22:43 +0100

    .

---
+++ modal8.js
@@ -0,0 +1,53 @@
+window.addEventListener("load", (event) => {
+  console.log("page is fully loaded");
+  var modal = document.getElementById("myModal");
+
+  var img1 = document.getElementById("img1");
+  var img2 = document.getElementById("img2");
+  var img3 = document.getElementById("img3");
+  var img4 = document.getElementById("img4");
+  var img5 = document.getElementById("img5");
+  var img6 = document.getElementById("img6");
+  var img7 = document.getElementById("img7");
+  var img8 = document.getElementById("img8");
+
+  var modalImg = document.getElementById("img01");
+
+  var span = document.getElementsByClassName("close")[0];
+  
+  img1.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img2.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img3.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img4.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img5.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img6.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img7.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  img8.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  };
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+});
